b57c75a1168d57a28a0675fe2cf53459e829fb8f
Vasya <vpetruin@inbox.ru>
2022-06-06 23:27:46 +0300

    initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.app}>
+      TimeMachine is working!!!
+    </div>
+  );
+}
+
+export default App;
